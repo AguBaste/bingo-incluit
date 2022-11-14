133ff98d317aa85947456f2698adc8aef19a2b06
AguBaste <agustinbasterrica@hotmail.com>
2022-11-14 17:48:27 -0300

    creacion de la lista con los numeros para el sorteo

--- Bingo/Bingo/wwwroot/js/site.js
+++ Bingo/Bingo/wwwroot/js/site.js
@@ -1,9 +1,22 @@
 ﻿
-const sortear = document.getElementById('sortear');
-let parrafo = document.getElementById('parrafo');
-function Sorteo() {
-    var numero = parseInt(Math.random() * 100);
 
-    console.log(numero);
-    parrafo.innerHTML = numero;
+let parrafo = document.getElementById('parrafo');
+
+const numeros = [];
+
+for (let index = 0; index < 90; index++) {
+  numeros[index] = index + 1;
 }
+
+numeros.sort(()=>Math.random()-0.5);
+let indice = 0;
+
+function Sorteo() {
+  while (indice < 90) {
+    parrafo.innerHTML = numeros[indice];
+    indice++;
+    return numeros[indice];
+  }
+  parrafo.innerHTML = "Se acabaron los numeros";
+}
+console.log(numeros);
